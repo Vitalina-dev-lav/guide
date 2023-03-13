53791988c46bbb0e1df3654984f653e1de7cbb0f
Vitalina <vitalinabatrakova@gmail.com>
2023-03-14 01:30:58 +0300

    Guid

---
+++ data_create.py
@@ -0,0 +1,15 @@
+def name_data():
+   name = input("Введите ваше имя: ")
+   return name 
+
+def surname_data():
+   surname = input("Введите вашу фамилию: ")
+   return surname 
+
+def telephone_data():
+   telephone = input("Введите ваш телефон: ")
+   return telephone 
+
+def adress_data():
+   adress = input("Введите ваш адрес: ")
+   return adress 
